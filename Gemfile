dbe401c86c93395897ceabbf48e17ff404b444a3
Seokho Song <0xdevssh@gmail.com>
2021-11-20 17:19:00 +0900

    Sat Nov 20 17:19:00 KST 2021

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source "https://rubygems.org"
+gem "webrick"
+gem 'jekyll-sitemap'
+gem 'jekyll-paginate' 
 gem "kramdown-parser-gfm"
+gem 'nokogiri', '~> 1.10.0'
 gem 'jekyll-multiple-languages-plugin'
 gemspec
---
+++ _site/ko/publish.sh
@@ -0,0 +1,13 @@
+#sh 
+#blish.sh
+
+git checkout gh-pages
+rm -rf _site/
+JEKYLL_ENV=production bundle exec jekyll build
+git add .
+git commit -m "`date` $1"
+git push stable gh-pages
+
+git subtree split --prefix _site/ -b master
+git push -f stable master
+git branch -D master
